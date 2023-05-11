f2a0b0e4a98d70d0e7142c436aa2828896fa6aa6
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-05-11 20:44:21 +0530

    Mongoose setup

--- server/index.js
+++ server/index.js
@@ -16,11 +16,11 @@ dotenv.config();
 const app = express();
 app.use(express.json);
 app.use(helmet());
-app.use(hemlet.crossOriginResourcePlolicy({policy : "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({policy : "cross-origin"}));
 app.use(morgan('common'));
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cros());
+app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 /* FILE STORAGE */
@@ -33,3 +33,10 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({storage: storage});
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))).catch((error) => console.log('${error} did not connect'));
