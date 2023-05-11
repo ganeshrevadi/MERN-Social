6f9085ca32b3ba93894dd7ba9798d677f4c94275
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-05-11 08:53:48 +0530

    Setting up multer storage

--- server/index.js
+++ server/index.js
@@ -14,4 +14,22 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json);
+app.use(helmet());
+app.use(hemlet.crossOriginResourcePlolicy({policy : "cross-origin"}));
+app.use(morgan('common'));
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cros());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/assets/images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({storage: storage});
