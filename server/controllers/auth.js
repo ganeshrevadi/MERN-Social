68699ee5420a29b916da4bf24f7fca337c49cf18
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-05-12 10:51:45 +0530

    Auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import User from '../models/user.js';
+import User from '../models/users.js';
 
 /* REGISTER */
 export const register = async (req, res) => {
@@ -11,7 +11,25 @@ export const register = async (req, res) => {
         const newUser = new User({ firstName, lastName, email, password: hashedPassword, picturePath,friends,location,occupation,viewedProfile:Math.floor(Math.random() * 1000),impressions:Math.floor(Math.random() * 1000) });
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
-    } catch (error) {
-        res.status(409).json({ message: error.message });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+/* LOGIN */
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if(!user) return res.status(400).json({ message: "User does not exist." });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({msg : "Invalid crendentials"});
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({ result: user, token });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 }
